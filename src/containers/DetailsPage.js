e690eab07ac9797d748f6bbff4f45368619e34ac
federuiz <ruizfederico95@gmail.com>
2017-07-04 13:05:03 -0300

    added style to details page

--- src/containers/DetailsPage.js
+++ src/containers/DetailsPage.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { ActionCreators as UndoActionCreators } from 'redux-undo'
-import {Col, Grid, Row} from "react-bootstrap";
+import {Col, Grid, Row, Jumbotron} from "react-bootstrap";
+import styles from './PlaceDetails.css'
 
 class DetailsPage extends Component {
 
     static propTypes = {
         canUndo : PropTypes.bool.isRequired,
-        selectedPlace: PropTypes.shape(
+        place: PropTypes.shape(
             {
                 title: PropTypes.string,
                 subTitle: PropTypes.string,
@@ -30,104 +31,74 @@ class DetailsPage extends Component {
         ),
         onUndo: PropTypes.func.isRequired
     };
-
     render(){
-        return(
+        const title =
             <div>
                 <button onClick={this.props.onUndo} disabled={!this.props.canUndo}>
                     Undo
                 </button>
-                <h1>{this.props.selectedPlace.title}</h1>
-                <br/>
-                <h3>{this.props.selectedPlace.subTitle}</h3>
-                <Grid>
-                    <Row>
+                <Row>
+                    <Col lg={12}>
+                        <h1>{this.props.place.title}</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={12}>
+                        <h3>{this.props.place.subTitle}</h3>
+                    </Col>
+                </Row>
+            </div>
+            ;
+
+        let rows = [
+            <Row key="datos">
+                <Col lg={12} md={12}>
+                    <h3>Datos de interes</h3>
+                </Col>
+            </Row>
+        ];
+        if (this.props.place.details) {
+            const regex = new RegExp("_", 'g');
+            for (let [k, v] of Object.entries(this.props.place.details)) {
+                rows.push(
+                    <Row key={k + v} className="detail-item">
                         <Col xs={6} md={6}>
-                            <ul>
-                                <li>
-                                    <h4>Comuna</h4>
-                                </li>
-                                <li>
-                                    <h4>Barrio</h4>
-                                </li>
-                                <li>
-                                    <h4>Comisaria</h4>
-                                </li>
-                                <li>
-                                    <h4>Area Hospitalaria</h4>
-                                </li>
-                                <li>
-                                    <h4>Region Sanitaria</h4>
-                                </li>
-                                <li>
-                                    <h4>Distrito Escolar</h4>
-                                </li>
-                                <li>
-                                    <h4>Seccion Catastral</h4>
-                                </li>
-                                <li>
-                                    <h4>Distrito Economico</h4>
-                                </li>
-                                <li>
-                                    <h4>Codigo de Planeamiento Urbano</h4>
-                                </li>
-                                <li>
-                                    <h4>Partido Amba</h4>
-                                </li>
-                                <li>
-                                    <h4>Localidad Amba</h4>
-                                </li>
-                            </ul>
+                            <h4>{k.replace(regex, " ").split(" ").map((d) => d[0].toUpperCase() + d.substr(1, d.length)).join(" ")}:</h4>
                         </Col>
                         <Col xs={6} md={6}>
-                            <ul>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.comuna}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.barrio}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.comisaria}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.area_hospitalaria}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.region_sanitaria}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.distrito_escolar}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.seccion_catastral}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.distrito_economico}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.codigo_de_planeamiento_urbano}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.partido_amba}</h4>
-                                </li>
-                                <li>
-                                    <h4>{this.props.selectedPlace.details.localidad_amba}</h4>
-                                </li>
-                            </ul>
+                            <h4 className="pull-right">{v}</h4>
                         </Col>
                     </Row>
+                )
+            }
+        }
+        else {
+            rows.push(
+                <Row>
+                    <Col lg={12}>
+                        <h4>No se han encontrado datos sobre el lugar</h4>
+                    </Col>
+                </Row>
+            )
+        }
+
+        return(
+            <div>
+                <Grid >
+                    {title}
+                    <Jumbotron>
+                        {rows}
+                    </Jumbotron>
                 </Grid>
             </div>
         )
     }
 
-
 }
 
 const mapStateToProps = (state) => ({
     canUndo: state.places.past.length > 0,
-    selectedPlace: state.places.present.selectedPlace
+    place: state.places.present.selectedPlace
 });
 
 const mapDispatchToProps = ({
